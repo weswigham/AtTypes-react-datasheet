356a531d95a7daaf766a5fed5b2fde3222c46b76
Wesley Wigham <wwigham@gmail.com>
2017-04-04 17:19:21 -0400

    Reflect actual exported structure of react-datasheet

--- index.d.ts
+++ index.d.ts
@@ -1,40 +1,42 @@
 import {PureComponent, Component} from "react";
 
-export interface Cell {
-    readOnly?: boolean;
-    key?: string | undefined;
-    className?: string | undefined;
-    component?: Component<any, any>;
-    forceComponent?: boolean;
-    colSpan?: number;
-    rowSpan?: number;
+declare namespace ReactDataSheet {
+    export interface Cell {
+        readOnly?: boolean;
+        key?: string | undefined;
+        className?: string | undefined;
+        component?: Component<any, any>;
+        forceComponent?: boolean;
+        colSpan?: number;
+        rowSpan?: number;
+    }
+
+    export interface DataSheetProps<T extends Cell> {
+        data: T[][];
+        className?: string;
+        valueRenderer: (cell: T) => string | number | void;
+        dataRenderer?: (cell: T) => string | number | void;
+        onChange?: (cell: T, i: number, j: number, newValue: string) => any;
+    }
+
+    export interface CellReference {
+        i: number;
+        j: number;
+    }
+
+    export interface DataSheetState {
+        start?: CellReference;
+        end?: CellReference;
+        selecting?: boolean;
+        forceEdit?: boolean;
+        editing?: CellReference;
+        clear?: CellReference;
+    }
 }
 
-export interface DataSheetProps<T extends Cell> {
-    data: T[][];
-    className?: string;
-    valueRenderer: (cell: T) => string | number | void;
-    dataRenderer?: (cell: T) => string | number | void;
-    onChange?: (cell: T, i: number, j: number, newValue: string) => any;
-}
-
-export interface CellReference {
-    i: number;
-    j: number;
-}
-
-export interface DataSheetState {
-    start?: CellReference;
-    end?: CellReference;
-    selecting?: boolean;
-    forceEdit?: boolean;
-    editing?: CellReference;
-    clear?: CellReference;
-}
-
-declare class ReactDataSheet<T extends Cell> extends PureComponent<DataSheetProps<T>, DataSheetState> {
-    getSelectedCells(data: T[][], start: CellReference, end: CellReference): {cell: T, i: number, j: number};
+declare class ReactDataSheet<T extends ReactDataSheet.Cell> extends PureComponent<ReactDataSheet.DataSheetProps<T>, ReactDataSheet.DataSheetState> {
+    getSelectedCells(data: T[][], start: ReactDataSheet.CellReference, end: ReactDataSheet.CellReference): {cell: T, i: number, j: number};
 
 }
 
-export default ReactDataSheet;
+export = ReactDataSheet;
